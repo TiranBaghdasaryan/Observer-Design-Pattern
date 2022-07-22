aaae69ea203be0da72f39cbfd155941c371b36c4
Tiran Baghdasaryan <tirbaghdasaryan0808@gmail.com>
2022-07-22 12:31:50 +0400

    initial commit

---
+++ Observer Design Pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Observer_Design_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
